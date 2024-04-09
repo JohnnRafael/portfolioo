5bfb31eafc2763a802653c50084300caa25fc97b
John Tugano <johnntugano@gmail.com>
2024-04-09 15:12:01 +0800

    Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import NavigationBar from './components/Navbar/NavigationBar'
 
 function App() {
+  const current_theme = localStorage.getItem('current_theme');
+  const [theme, setTheme] = useState(current_theme? current_theme : 'light');
+
+  useEffect(()=>{
+    localStorage.setItem('current_theme', theme)
+  }, [theme])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = {`container ${theme}`}>
+      <NavigationBar theme={theme} setTheme={setTheme}/>
     </div>
-  );
+   
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/NavigationBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './NavigationBar.css'
+import logo_light from '../../assets/logo-black.png'
+import logo_dark from '../../assets/logo-white.png'
+import toggle_light from '../../assets/night.gif'
+import toggle_dark from '../../assets/day.png'
+
+
+const NavigationBar = ({theme, setTheme}) => {
+
+    const toggle_mode = ()=>{
+        theme == 'light' ? setTheme('dark') : setTheme('light');
+    }
+  return (
+    <div className = 'NavBar'>
+        <img src={theme == 'light' ? logo_light : logo_dark} alt= "" className='logo'/>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>      
+        <img onClick={()=>{toggle_mode()}} src={theme == 'light' ? toggle_light : toggle_dark} alt= "" className='toggle-icon'/>
+    </div>
+  )
+}
+
+export default NavigationBar
